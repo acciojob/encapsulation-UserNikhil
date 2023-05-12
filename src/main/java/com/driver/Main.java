dd5b02a78a25b7e389008ba162d0520507f6c4e2
Vinod Kumar <neelavinod26@gmail.com>
2023-05-12 19:30:18 +0530

    Encapsulation Example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
-}
+  public static void main(String[] args)
+  {
+    RWOnly obj=new RWOnly();
+
+    obj.setName("Nikhil");
+
+    String name=obj.getName();gi
+
+    System.out.println(name);
+  }
+}l
